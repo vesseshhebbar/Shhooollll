a58e1f3d3f3c8d020022cc326277e6916757972e
Vessesh Hebbar <33597053+vesseshhebbar@users.noreply.github.com>
2019-10-16 11:41:59 +0530

    WASD_in game

--- Python/game_in_py.py
+++ Python/game_in_py.py
@@ -8,6 +8,8 @@ Created on Fri Oct 11 09:22:06 2019
 import pygame
 pygame.init()
 
+
+
 win = pygame.display.set_mode((500, 500))  
 pygame.display.set_caption("asd")
 
@@ -24,21 +26,26 @@ while(run):
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
             run = False;
-
+        
     keys = pygame.key.get_pressed()
     
-    if(keys[pygame.K_LEFT]):
-        x -= vel
-    if(keys[pygame.K_RIGHT]):
+    if(keys[pygame.K_a]):
+        if(x > 0):
+            x -= vel
+    if(keys[pygame.K_d]):
+        if(x < 500-width-0):
             x += vel
-    if(keys[pygame.K_UP]):
+    if(keys[pygame.K_w]):
+        if(y > 0):
             y -= vel
-    if(keys[pygame.K_DOWN]):
+    if(keys[pygame.K_s]):
+        if(y < 500-height-0):
             y += vel
+
     
-    win.fill((0,0,0))          
+    win.fill((0,0,0))
     pygame.draw.rect(win, (255,0,0), (x,y,width,height))
     pygame.display.update()
-            
+
 pygame.quit()
         
